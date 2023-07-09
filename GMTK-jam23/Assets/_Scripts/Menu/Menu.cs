44ea5206ebda06e0ff1f456f59a1c97ae0f35453
monninen <79700542+monninen@users.noreply.github.com>
2023-07-09 12:50:04 +0300

    Puolalaistamista

--- GMTK-jam23/Assets/_Scripts/Menu/Menu.cs
+++ GMTK-jam23/Assets/_Scripts/Menu/Menu.cs
@@ -5,18 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] Animator anim;
     public void PlayGame()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene(1);
-        SceneManager.LoadScene(2, LoadSceneMode.Additive);
-        SceneManager.LoadScene(3, LoadSceneMode.Additive);
+        StartCoroutine(LoadNextScene());
+        anim.SetTrigger("Trans");
     }
 
     public void RestartLevel()
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(0.4f);
+        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
+        SceneManager.LoadScene(3, LoadSceneMode.Additive);
     }
 
 }
