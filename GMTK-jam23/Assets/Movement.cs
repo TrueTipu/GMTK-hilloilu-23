d0aa511ce00baef8230367e5897a71f210e23afb
TrueTipu <59774591+TrueTipu@users.noreply.github.com>
2023-07-09 19:47:05 +0300

    tässä

--- GMTK-jam23/Assets/Movement.cs
+++ GMTK-jam23/Assets/Movement.cs
@@ -6,13 +6,31 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] GameObject particles;
+    [SerializeField] SpriteRenderer sp;
+    [SerializeField] SpriteRenderer sp2;
+    bool wait = true;
     private void Start()
     {
         MusicPlayer.Instance.SetSourceActive(0);
         AudioManager.Instance.Play("EndTheme");
+        StartCoroutine(TransSukupuolinenIhminen());
+    }
+
+    IEnumerator TransSukupuolinenIhminen()
+    {
+        Debug.Log("a");
+        while (sp.color.a < 1)
+        {
+            Debug.Log("b");
+            sp.color = new Color(sp.color.r, sp.color.g, sp.color.b, sp.color.a + 0.01f);
+            sp2.color = new Color(sp2.color.r, sp2.color.g, sp2.color.b, sp2.color.a - 0.01f);
+            yield return new WaitForSeconds(0.01f);
+        }
+        wait = false;
     }
     void Update()
     {
+        if (wait) return;
         if (Input.GetKey(KeyCode.Space))
         {
             transform.position -= new Vector3(0, speed * Time.deltaTime);
